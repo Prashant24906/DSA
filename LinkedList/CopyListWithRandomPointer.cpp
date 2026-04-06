4d204576dbd9fe19583fa770324d12a34ad3ca99
parshu2496 <prashantnrwd@gmail.com>
2026-04-06 16:20:14 +0530

    Copying the linked list with random

--- LinkedList/CopyListWithRandomPointer.cpp
+++ LinkedList/CopyListWithRandomPointer.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
 using namespace std;
-class Node{
-    public:
+
+/*
+// Definition for a Node.
+class Node {
+public:
     int val;
     Node* next;
     Node* random;
-    Node(int val){
-        this->val = val;
-        this->next = NULL;
-        this->random = NULL;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
 };
-int main()
+*/
 
-{
+class Solution {
+public:
+    // O(N) time and space complexity
+    Node* copyRandomList(Node* head) {
     Node* curr = head;
         unordered_map<Node* ,Node* > old_to_new;
         while(curr){
@@ -27,4 +34,39 @@ int main()
             curr = curr->next;
         }
         return old_to_new[head];
-}
+    }
+};
+
+class Solution {
+public:
+    // O(N) time and O(1) space complexity
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+        Node* mover = head;
+        while(mover){
+            Node* temp = new Node(mover->val);
+            temp->next = mover->next;
+            mover->next = temp;
+            mover = temp->next;
+        }
+        //assign random 
+        mover = head;
+        while(mover){
+            if(mover->random)
+                mover->next->random = mover->random->next;
+            mover = mover->next->next;
+        }
+        Node* newHead = head->next;
+        mover = head;
+        Node* mover2 = newHead;
+        while(mover&&mover2){
+            mover->next = mover->next->next;
+            if(mover2->next)
+                mover2->next = mover2->next->next;
+            mover = mover->next;
+            mover2 = mover2->next;
+        }
+        return newHead;
+        
+    }
+};
