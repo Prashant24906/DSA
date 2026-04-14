24ba24b5ce276dcb596ffd4713682f936004c920
parshu2496 <prashantnrwd@gmail.com>
2026-04-14 18:58:57 +0530

    Check if there exists a subsequence with sum K

---
+++ Recursion/Subsequence Pattern/4) Count all subsequences with sum K.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+        int helper(vector<int>& nums,int target,int sum,int index){
+        int ans = 0;
+        if(index==nums.size()||sum>target){
+            if(sum==target) return 1;
+            else return 0;
+        }
+        sum+=nums[index];
+        ans+=helper(nums,target,sum,index+1);
+        sum-=nums[index];
+        ans+=helper(nums,target,sum,index+1);
+        return ans;
+    }
+    int numSubseq(vector<int>& nums, int target) {
+        int ans = 0;
+        ans+=helper(nums,target,0,0);
+        return ans;
+    }
+};
