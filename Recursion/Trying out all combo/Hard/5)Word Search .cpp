37b20a3079817bd13b517190f6868959cba31a6f
parshu2496 <prashantnrwd@gmail.com>
2026-05-06 17:00:47 +0530

    Word Search

---
+++ Recursion/Trying out all combo/Hard/5)Word Search .cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool helper(string s,vector<string>& wordDict,int index){
+        if(index==s.size()) return true;
+        if(index>s.size()) return false;
+        for(int i = 0;i<wordDict.size();i++){
+            if(wordDict[i]==s.substr(index,wordDict[i].size())){
+            bool ans = helper(s,wordDict,index+wordDict[i].size());
+            if(ans==true) return true;
+            }
+
+        }
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        return helper(s,wordDict,0);
+    }
+};
