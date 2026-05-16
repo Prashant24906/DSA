d71062659b13cec700a9dc391c746dcc4d7677c5
Prashant <prashantnrwd@gmail.com>
2026-05-16 17:24:33 +0530

    Create 7) valid paranthesis.cpp

---
+++ 09_Stack_and_Queues/Implementation/7) valid paranthesis.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        unordered_map<char,char> mapping = {{')','('},{']','['},{'}','{'}};
+        for(char c: s){
+            if(mapping.find(c)==mapping.end())  
+                st.push(c);
+            else if(!st.empty()&&st.top()==mapping[c]) st.pop();
+            else return false;
+        }
+        return st.empty();
+    }
+};
