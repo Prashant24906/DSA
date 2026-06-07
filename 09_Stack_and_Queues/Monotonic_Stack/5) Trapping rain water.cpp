ca15e6f5011ebb63bdf494f1c30fdc760cb23181
Prashant <prashantnrwd@gmail.com>
2026-06-07 18:21:05 +0530

    Create 5) Trapping rain water.cpp

---
+++ 09_Stack_and_Queues/Monotonic_Stack/5) Trapping rain water.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        int left = 0;
+        int right = n - 1;
+
+        int maxLeft = 0;
+        int maxRight = 0;
+
+        int totalWater = 0;
+        while (left <= right) {
+            if(height[left]<=height[right]){
+                if(maxLeft<height[left]){
+                    maxLeft = height[left];
+                }
+                else{
+                    totalWater+=maxLeft-height[left];
+                    left++;
+                }
+            }
+            else{
+                if(maxRight<height[right]){
+                    maxRight = height[right];
+                }
+                else{
+                    totalWater+=maxRight-height[right];
+                    right--;
+                }
+            }
+        }
+        return totalWater;
+    }
+};
