d900b00fb488ef123766b2a19f73c200cf3bf6fb
Prashant <prashantnrwd@gmail.com>
2026-06-08 22:00:14 +0530

    Create 6)Sum of Subarray Minimums.cpp

---
+++ 09_Stack_and_Queues/Monotonic_Stack/6)Sum of Subarray Minimums.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> NextSmall(vector<int>& arr,vector<int>& NextSmaller){
+        stack<int> st;
+        for(int i = arr.size()-1;i>=0;i--){
+            while(!st.empty()&&arr[st.top()]>=arr[i]) st.pop();
+            if(!st.empty())
+                NextSmaller[i] = st.top();
+            else    
+                NextSmaller[i] = arr.size();
+            st.push(i);
+        }
+        return NextSmaller;
+    }
+    vector<int> PrevSmall(vector<int>& arr,vector<int>& PrevSmaller){
+        stack<int> st;
+        for(int i = 0;i<arr.size();i++){
+            while(!st.empty()&&arr[st.top()]>arr[i]) st.pop();
+            if(!st.empty())
+                PrevSmaller[i] = st.top();
+            else    
+                PrevSmaller[i] = -1;
+            st.push(i);
+        }
+        return PrevSmaller;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        long long res = 0;
+        int MOD = 1e9 + 7;
+        int n = arr.size();
+        stack<int> st;
+        vector<int> NextSmaller(arr.size(),0);
+        vector<int> PrevSmaller(arr.size(),0);
+        NextSmall(arr,NextSmaller);
+        PrevSmall(arr,PrevSmaller);
+        for(int i = 0;i<arr.size();i++){
+            long long prod = ((i-PrevSmaller[i])*(NextSmaller[i]-i))%MOD;
+            prod = (prod*arr[i])%MOD;
+            res = (res + prod)%MOD;
+        }   
+        return res;
+    }
+};
