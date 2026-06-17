f0394d19fe0f4d897dd832bd48f45095281a3fcb
Prashant <prashantnrwd@gmail.com>
2026-06-17 19:38:39 +0530

    Create 2)Stock span problem.cpp

---
+++ 09_Stack_and_Queues/Implementation/2)Stock span problem.cpp
@@ -0,0 +1,21 @@
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    StockSpanner() {
+        
+    }
+    int next(int price) {
+        int greater = 1;
+        while(!st.empty()&&st.top().first<=price){
+            greater+=st.top().second;
+            st.pop();
+        }
+        st.push({price,greater});
+        return greater;
+    }
+};
