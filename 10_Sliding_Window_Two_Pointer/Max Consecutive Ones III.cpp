6a9ab843e5a96f8a3b2d91290a6889a4541508f3
Prashant <prashantnrwd@gmail.com>
2026-06-19 19:36:54 +0530

    Update Max Consecutive Ones III.cpp

--- 10_Sliding_Window_Two_Pointer/Max Consecutive Ones III.cpp
+++ 10_Sliding_Window_Two_Pointer/Max Consecutive Ones III.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-        int l = 0,r = 0,maxi = 0;
+        int l = 0;
         int zeros = 0;
-        while(r<n){
-            if(zeros==k&&nums[r]==0){
-                while(nums[l] == 1&& l<n)l++;
-                l++;
-                zeros--;
-                continue; 
-            }
-            if(nums[r]==0){
+        int ans = 0;
+
+        for(int r = 0; r < nums.size(); r++) {
+
+            if(nums[r] == 0)
                 zeros++;
+
+            while(zeros > k) {
+                if(nums[l] == 0)
+                    zeros--;
+                l++;
             }
-            maxi = max(maxi,r-l+1);
-            r++;
+
+            ans = max(ans, r - l + 1);
         }
-        return maxi;
+
+        return ans;
     }
 };
