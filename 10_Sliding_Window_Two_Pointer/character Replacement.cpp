5ade1d7b70470e304dfe64daa49223728f36a652
Prashant <prashantnrwd@gmail.com>
2026-06-20 13:55:49 +0530

    Create character Replacement.cpp

---
+++ 10_Sliding_Window_Two_Pointer/character Replacement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int ans = 0;
+        unordered_map<char,int> mp;
+        int maxfreq = 0;
+        int temp = k;
+        for(int r = 0; r < s.size(); r++) {
+            mp[s[r]]++;
+            maxfreq = max(maxfreq,mp[s[r]]);
+            while(r-l+1-maxfreq>k){
+                mp[s[l]]--;
+                l++;
+            }
+            ans = max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
