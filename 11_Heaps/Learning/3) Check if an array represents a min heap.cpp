6f2bb60097c791ca4c7d41e54ab01d4c709ac94a
Prashant <prashantnrwd@gmail.com>
2026-07-08 22:10:57 +0530

    Update 3) Check if an array represents a min heap.cpp

--- 11_Heaps/Learning/3) Check if an array represents a min heap.cpp
+++ 11_Heaps/Learning/3) Check if an array represents a min heap.cpp
@@ -11,7 +11,6 @@ Explanation: The input represents a min-heap.
 	  / \	  /
 	 /   \   /
 	5	 10 15
-
 Input : [2, 10, 4, 5, 3, 15]
 Output: false
 Explanation: The input is not a min-heap, as it violate the heap property.
