90658c7d4ea0af576305dfb84be8698ec2c88ee7
Prashant <prashantnrwd@gmail.com>
2026-07-28 22:39:20 +0530

    Update 1) Assign Cookies.cpp

--- 12_Greedy/Easy/1) Assign Cookies.cpp
+++ 12_Greedy/Easy/1) Assign Cookies.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = g.size(),m = s.size();
+        int n = g.size();
+        int m = s.size();
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         int i = 0,j = 0,count = 0;
