9ffd9241716c75046b08f7ac5250f23cc9e6df64
Prashant <prashantnrwd@gmail.com>
2026-07-29 21:54:20 +0530

    Update 3) lemonade Change.cpp

--- 12_Greedy/Easy/3) lemonade Change.cpp
+++ 12_Greedy/Easy/3) lemonade Change.cpp
@@ -3,9 +3,8 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int five = 0,ten = 0;
         for(int i = 0;i<bills.size();i++){
-            if(bills[i]==5){
+            if(bills[i]==5)
                 five++;
-            }
             else if(bills[i]==10){
                 ten++;
                 if(five==0) return false;
