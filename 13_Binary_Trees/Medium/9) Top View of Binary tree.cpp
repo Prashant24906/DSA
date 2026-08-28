e15a560c97865414c61f5e9cffe15cedcd45d9ee
Prashant <prashantnrwd@gmail.com>
2026-08-28 14:43:11 +0530

    Create 9) Top View of Binary tree.cpp

---
+++ 13_Binary_Trees/Medium/9) Top View of Binary tree.cpp
@@ -0,0 +1,33 @@
+class Solution {
+	public:
+	vector<int> levelOrder(Node* root) {
+		if (root == nullptr) {
+			return {};
+		}
+		vector<int> ans;
+		map<int, int> mp;
+		queue<pair<Node*, int>> q;
+		q.push({root,0});
+		while (!q.empty()) {
+			int line = q.front().second;
+			Node* node = q.front().first;
+			q.pop();
+			if(mp.find(line)==mp.end()){
+			    mp[line] = node->data;
+			}
+			if (node->left)
+				q.push({node->left,line-1});
+			
+			if (node->right)
+				q.push({node->right,line+1});
+			
+		}
+		for(auto it:mp){
+		    ans.push_back(it.second);
+		}
+		return ans;
+	}
+	vector<int> topView(Node *root) {
+		return levelOrder(root);
+	}
+};
